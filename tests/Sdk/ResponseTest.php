c7b8ae33c283cd42651a3554c6b95675f7458fa8
Julian Li <weiyi.li713@gmail.com>
2018-05-28 11:26:44 +1000

    response test

---
+++ tests/Sdk/ResponseTest.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\LoyaltyCorp\SdkBlueprint\Sdk;
+
+use LoyaltyCorp\SdkBlueprint\Sdk\Response;
+use Tests\LoyaltyCorp\SdkBlueprint\TestCase;
+
+class ResponseTest extends TestCase
+{
+    /**
+     * @var Response
+     */
+    private $response;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->response = new Response(200, ['reponse' => 'data'], '1', 'success');
+    }
+
+    public function testGetStatusCode(): void
+    {
+        self::assertSame(200, $this->response->getStatusCode());
+    }
+
+    public function testGetMessage(): void
+    {
+        self::assertSame('success', $this->response->getMessage());
+    }
+
+    public function testGetCode(): void
+    {
+        self::assertSame(1, $this->response->getCode());
+    }
+
+    public function testGetContent(): void
+    {
+        self::assertSame(['response' => 'data'], $this->response->getContent());
+    }
+}
