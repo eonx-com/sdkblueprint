bf38e4f642f583c36f6176480fdadb0f1be287eb
Julian Li <weiyi.li713@gmail.com>
2018-06-18 14:45:15 +1000

    add magic getter and setter to access protect attribute

--- src/Sdk/BaseDataTransferObject.php
+++ src/Sdk/BaseDataTransferObject.php
@@ -4,11 +4,22 @@ declare(strict_types=1);
 namespace LoyaltyCorp\SdkBlueprint\Sdk;
 
 use EoneoPay\Externals\ORM\Entity;
+use EoneoPay\Utils\Str;
 
 class BaseDataTransferObject extends Entity
 {
+    public function __get($property)
+    {
+        return $this->$property;
+    }
+
+    public function __set($property, $value)
+    {
+        $this->$property = $value;
+    }
+
     /**
-     * {@inheritdoc}
+     * {@inheritdoc
      */
     public function toArray(): array
     {
