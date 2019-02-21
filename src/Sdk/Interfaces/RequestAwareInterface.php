e310f43bac34b0b04f74f98bee852a799a0c52ba
Ketan Padia <ketan@loyaltycorp.com.au>
2019-02-21 16:41:11 +1100

    fix lint errors

--- src/Sdk/Interfaces/RequestAwareInterface.php
+++ src/Sdk/Interfaces/RequestAwareInterface.php
@@ -6,29 +6,33 @@ namespace LoyaltyCorp\SdkBlueprint\Sdk\Interfaces;
 interface RequestAwareInterface
 {
     /**
+     * The create method.
+     *
+     * @var string
+     */
+    public const CREATE = 'post';
+
+    /**
      * The delete method.
      *
      * @var string
      */
     public const DELETE = 'delete';
+
     /**
      * The get method.
      *
      * @var string
      */
     public const GET = 'get';
+
     /**
      * The list method.
      *
      * @var string
      */
     public const LIST = 'list';
-    /**
-     * The create method.
-     *
-     * @var string
-     */
-    public const CREATE = 'post';
+
     /**
      * The update method.
      *
