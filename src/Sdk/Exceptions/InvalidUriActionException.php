5a8c7009fee7d213640bb186d84390f052a1f89e
Brendon Tyree <brendon@tyree.co.nz>
2019-08-14 11:44:05 +1000

    PYMT-1170: Updated request handler to throw an exception for an invalid action, updated tests

---
+++ src/Sdk/Exceptions/InvalidUriActionException.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace LoyaltyCorp\SdkBlueprint\Sdk\Exceptions;
+
+use EoneoPay\Utils\Exceptions\ValidationException;
+
+/**
+ * An exception that is thrown when the request action does not exist under an entities supported URIs.
+ */
+class InvalidUriActionException extends ValidationException
+{
+    /**
+     * @inheritdoc
+     */
+    public function getErrorCode(): int
+    {
+        return self::DEFAULT_ERROR_CODE_VALIDATION;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getErrorSubCode(): int
+    {
+        return self::DEFAULT_ERROR_SUB_CODE + 1;
+    }
+}
---
+++ tests/Sdk/Exceptions/InvalidUriActionExceptionTest.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\LoyaltyCorp\SdkBlueprint\Sdk\Exceptions;
+
+use EoneoPay\Utils\Interfaces\BaseExceptionInterface;
+use LoyaltyCorp\SdkBlueprint\Sdk\Exceptions\InvalidUriActionException;
+use Tests\LoyaltyCorp\SdkBlueprint\TestCase;
+
+/**
+ * @covers \LoyaltyCorp\SdkBlueprint\Sdk\Exceptions\InvalidUriActionException
+ */
+class InvalidUriActionExceptionTest extends TestCase
+{
+    /**
+     * Test that exception has expected code.
+     *
+     * @return void
+     */
+    public function testExceptionCodes(): void
+    {
+        $exception = new InvalidUriActionException();
+
+        self::assertSame(BaseExceptionInterface::DEFAULT_ERROR_CODE_VALIDATION, $exception->getErrorCode());
+        self::assertSame(1, $exception->getErrorSubCode());
+    }
+}
